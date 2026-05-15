de4b3d447ad9ae77a3c0c2c60c0bfdefbd5186c8
vinoth9598 <pvinoth9598@gmail.com>
2026-05-15 14:36:12 +0530

    return method

--- src/Methods_demo.java
+++ src/Methods_demo.java
@@ -1,36 +1,14 @@
 
 public class Methods_demo {
-    static void myMethod(){
-        System.out.println("I just got executed");
-    }
     public static void main(String[] args){
-        myMethod();
-        myMethod();
-        myMethod();
 
-        //parameter and arguments
-        myMethods("Liam");
-        myMethods("Jenny");
+        //return method
+       int result = myMethod(10, 30);
+       System.out.println(result);
 
-        //parameters
-        myMethods1("vinoth", 21);
-
-        checkAge(20);
     }
 
-    static void myMethods(String name ){
-        System.out.println(name + " Refsnes");
-    }
-
-    static void myMethods1(String name, int age){
-        System.out.println("Name :" + name + " Age :" + age);
-    }
-
-    static void checkAge(int age) {
-        if(age < 18){
-            System.out.println("Access denied - You are not old enough");
-        }else {
-            System.out.println("Access granted - you are old enough!");
-        }
+    static int myMethod(int a, int b){
+        return (a + b);
     }
 }
